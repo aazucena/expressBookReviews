741ba7344d8db87f23001c96f36c8289a28596f5
aazucena <aldrinp.azucena@gmail.com>
2025-08-06 02:22:26 -0600

    Add Playwright testing

---
+++ final_project/playwright.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    // All requests we send go to this API endpoint.
+    baseURL: 'http://localhost:5000',
+  }
+});
